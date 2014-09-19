d7e922eeb16ada8b9286b56cf5ab866641f1b9ea
netheril96 <netheril96@gmail.com>
2014-09-19 14:47:12 +0800

    Output the temporary test program to /tmp

--- do_test.sh
+++ do_test.sh
@@ -3,6 +3,7 @@
 git submodule init
 git submodule update
 python3 autojsoncxx.py examples/userdef.json --out=test/userdef.hpp
-c++ -O2 -Wall -Wextra -pedantic -std=c++11 -I./include -I./rapidjson/include -I./catch/single_include test/main.cpp test/test.cpp -o autojsoncxx_test
-./autojsoncxx_test --success
-rm autojsoncxx_test
+
+mkdir -p /tmp/26EB22B7-6CD9-4465-A5DE-04205274EE51/
+c++ -O2 -Wall -Wextra -pedantic -std=c++11 -I./include -I./rapidjson/include -I./catch/single_include test/main.cpp test/test.cpp -o /tmp/26EB22B7-6CD9-4465-A5DE-04205274EE51/test_autojsoncxx
+/tmp/26EB22B7-6CD9-4465-A5DE-04205274EE51/test_autojsoncxx --success
